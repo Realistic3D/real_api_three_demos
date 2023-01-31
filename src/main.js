19011450f855cc077b226e90d41e77a2a115a2b5
USMAN HAMEED <usmanham33d@live.com>
2023-02-01 00:09:32 +0800

    feat: scripts added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Scene from "./js/tools/Scene.js";
 import {LoadExample1} from "./examples/loadExample1.js";
+import {RenderThree} from "./render/RenderThree.js";
 
 
 async function Start() {
     const scene = new Scene();
     await LoadExample1(scene);
-    console.log(scene);
+    // console.log(scene);
+    await RenderThree(scene);
     return 1;
 }
 
--- src/render/RenderThree.js
+++ src/render/RenderThree.js
@@ -1,5 +1,19 @@
+// import * as RealAPI from "../public/js/RealAPI.js"
 
+export async function RenderThree(scene) {
+    // console.log(RealAPI);
+}
 
-export async function RenderThree() {
-    //
+function GetApiCredential() {
+    // const product = new RealProduct(
+    //     "Real3D",
+    //     "HF0mRDz6R60EIwEMxZit",
+    //     0
+    // );
+    // return new REAL.RealAPI(
+    //     "Real3D",
+    //     "HDQ8ygWidtm9yosQ270m",
+    //     "0HYZ77ntJpnHxInjtBh3",
+    //     product
+    // );
 }
