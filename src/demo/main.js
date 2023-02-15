f6589729933df3ddf6d836d424da733ca664cd63
USMAN HAMEED <usmanham33d@live.com>
2023-02-16 02:21:28 +0800

    fix: export issue

--- src/demo/main.js
+++ src/demo/main.js
@@ -1,13 +1,15 @@
 import Scene from "./tools/Scene.js";
 import {LoadExample1} from "./examples/loadExample1";
 import {LoadExample2} from "./examples/loadExample2";
+import {LoadExample3} from "./examples/LoadExample3";
 import {RenderThree} from "./render/render_three";
 
 
 export async function Start(canvas) {
     const scene = new Scene(canvas);
     // await LoadExample1(scene);
-    await LoadExample2(scene);
+    // await LoadExample2(scene);
+    await LoadExample3(scene);
     await RenderThree(scene);
     return 1;
 }
