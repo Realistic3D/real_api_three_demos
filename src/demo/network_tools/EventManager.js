45e15e2cc2a6537ab8f432ca716fecef1b4ad43e
USMAN HAMEED <usmanham33d@live.com>
2023-05-19 09:58:29 +0800

    ref: new interface

--- src/demo/network_tools/EventManager.js
+++ src/demo/network_tools/EventManager.js
@@ -3,14 +3,33 @@ export class EventManager {
     constructor(app) {
         this.app = app;
         this.result = null;
+        this.isMaximized = false;
         this.image = document.getElementById('result');
+        this.maxMinBtn = document.getElementById('maxMinBtn');
         this.imageViewer = document.querySelector('.image-viewer');
-        const maximizeBtn = document.getElementById('maximizeBtn');
-        const minimizeBtn = document.getElementById('minimizeBtn');
+        // const maximizeBtn = document.getElementById('maximizeBtn');
+        // const minimizeBtn = document.getElementById('minimizeBtn');
+        // maximizeBtn.addEventListener('click', this.maximizeImage.bind(this));
+        // minimizeBtn.addEventListener('click', this.minimizeImage.bind(this));
         const closeBtn = document.getElementById('closeBtn');
-        maximizeBtn.addEventListener('click', this.maximizeImage.bind(this));
-        minimizeBtn.addEventListener('click', this.minimizeImage.bind(this));
         closeBtn.addEventListener('click', this.closeImage.bind(this));
+        this.maxMinBtn.addEventListener('click', this.toggleImageSize.bind(this));
+    }
+    toggleImageSize() {
+        const img = this.image;
+        if (this.isMaximized) {
+            // Minimize image
+            this.imageViewer.style.maxHeight = "100%";
+            this.maxMinBtn.textContent = "Full view";
+        } else {
+            // Maximize image
+            this.imageViewer.style.removeProperty("max-height");
+            this.imageViewer.width = `${img.naturalWidth/2}px`;
+            this.imageViewer.height = `${img.naturalHeight/2}px`;
+            this.maxMinBtn.textContent = "Small view";
+        }
+
+        this.isMaximized = !this.isMaximized;
     }
     showImage(img) {
         if(!img) return;
@@ -18,15 +37,15 @@ export class EventManager {
         this.image.src = img;
         this.imageViewer.style.display = 'block';
     }
-    maximizeImage() {
-        this.imageViewer.style.width = '100%';
-        this.imageViewer.style.height = '100%';
-    }
-
-    minimizeImage() {
-        this.imageViewer.style.width = '300px';
-        this.imageViewer.style.height = 'auto';
-    }
+    // maximizeImage() {
+    //     this.imageViewer.style.width = '100%';
+    //     this.imageViewer.style.height = '100%';
+    // }
+    //
+    // minimizeImage() {
+    //     this.imageViewer.style.width = '300px';
+    //     this.imageViewer.style.height = 'auto';
+    // }
     closeImage() {
         this.result = null;
         this.imageViewer.style.display = 'none';
