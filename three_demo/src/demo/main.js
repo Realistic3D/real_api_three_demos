7ebe01931a40166c866050f243c5d44509843c69
USMAN HAMEED <usmanham33d@live.com>
2023-06-11 06:02:21 +0800

    ref: update

--- three_demo/src/demo/main.js
+++ three_demo/src/demo/main.js
@@ -1,17 +1,10 @@
 import Scene from "./tools/Scene.js";
-import {RenderThree} from "./render/render_three";
-import {ClassRoom} from "./room/classroom";
 import {House} from "./room/house";
-import {TestAny} from "./test/test_any";
 
 
 export async function Start(that) {
     const canvas = that.$refs.canvas;
-    const scene = new Scene(canvas);
-    that.scene = scene;
+    that.scene = new Scene(canvas);
     await House(that);
-    // await ClassRoom(that);
-    // await RenderThree(scene);
-    // await TestAny(scene);
     return 1;
 }
